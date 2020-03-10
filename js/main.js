d4b5c9e903af9b126399d9c653268689c13a2b8e
Beatrizfleon <Beatrizfleon@gmail.com>
2020-03-10 10:32:43 +0100

    added new conditionals

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ console.log(randomNumber);
 
 //Registro la función "listener" sobre el elemento a escuchar con una función "handler" que definiré después
 testButton.addEventListener('click', myFunction);
+const clue = document.querySelector('.clue');
 
 function myFunction() {
   console.log('mi función funciona?');
@@ -19,6 +20,16 @@ function myFunction() {
   const myInput = document.querySelector('.js-myInput');
   console.log(myInput.value);
   if (randomNumber === parseInt(myInput.value)) {
-    console.log('has ganado campeona!');
+    // console.log('Has ganado campeona!');
+    clue.innerHTML = 'has ganado campeona!';
+  } else if (randomNumber > parseInt(myInput.value)) {
+    // console.log('Demasiado bajo');
+    clue.innerHTML = 'Demasiado bajo';
+  } else if (randomNumber < parseInt(myInput.value)) {
+    // console.log('Demasiado alto');
+    clue.innerHTML = 'Demasiado alto';
+  } else if (parseInt(myInput.value) > 100 || parseInt(myInput.value) < 1) {
+    console.log('El número debe estar entre 1 y 100');
+    clue.innerHTML = 'El número debe estar entre 1 y 100';
   }
 }
