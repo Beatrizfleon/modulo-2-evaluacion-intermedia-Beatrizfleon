a2b83c02845031ab8544c788a64aa80f9b5dbc50
Beatrizfleon <Beatrizfleon@gmail.com>
2020-03-23 13:24:45 +0100

    replace innerHTMLclue by a function

--- js/main.js
+++ js/main.js
@@ -14,23 +14,33 @@ console.log(randomNumber);
 testButton.addEventListener('click', myFunction);
 const clue = document.querySelector('.clue');
 
-function myFunction() {
-  console.log('mi función funciona?');
+function myFunction(ev) {
+  ev.preventDefault();
+  giveClue();
 
+  // console.log('mi función funciona?');
+}
+
+function giveClue() {
   const myInput = document.querySelector('.js-myInput');
   console.log(myInput.value);
-  if (randomNumber === parseInt(myInput.value)) {
+  const myInputValue = parseInt(myInput.value);
+  if (randomNumber === myInputValue) {
     // console.log('Has ganado campeona!');
-    clue.innerHTML = 'has ganado campeona!';
-  } else if (randomNumber > parseInt(myInput.value)) {
+    paintClue('has ganado campeona!');
+  } else if (myInputValue > 100 || myInputValue < 1) {
+    // console.log('El número debe estar entre 1 y 100');
+    paintClue('El número debe estar entre 1 y 100');
+  } else if (randomNumber > myInputValue) {
     // console.log('Demasiado bajo');
-    clue.innerHTML = 'Demasiado bajo';
-  } else if (randomNumber < parseInt(myInput.value)) {
+    paintClue('Demasiado bajo');
+  } else if (randomNumber < myInputValue) {
     // console.log('Demasiado alto');
-    clue.innerHTML = 'Demasiado alto';
-  } else if (parseInt(myInput.value) > 100 || parseInt(myInput.value) < 1) {
-    console.log('El número debe estar entre 1 y 100');
-    clue.innerHTML = 'El número debe estar entre 1 y 100';
+    paintClue('Demasiado alto');
+  }
+  //sacar a una función el innerHTML
+
+  function paintClue(clueMessage) {
+    clue.innerHTML = clueMessage;
   }
 }
-function numberTries =
